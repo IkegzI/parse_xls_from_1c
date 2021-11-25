4a7aca820a29835eef1293ebe3eac343e39ec377
IkegzI <33818138+IkegzI@users.noreply.github.com>
2021-11-25 21:13:43 +0800

    initial

---
+++ xls_parse.rb
@@ -0,0 +1,40 @@
+require "roo-xls"
+
+class Parserxls
+  def initialize(file_xls)
+    @file = Roo::Excel.new file_xls
+    @hash ||= {}
+    @hash_att ||= {}
+  end
+
+  def parse
+    @file.each_with_index do |cell, index|
+      index -= 1
+      case index
+      when 8
+        cell.each_with_index { |item, index_line| @hash_att[index_line - 1] = item unless item.nil? }
+      when 9..cell.size
+        @hash_att.each do |key, value|
+          @hash[cell[0]] ||= {}
+          @hash[cell[0]][value] = cell[key] if key > 0
+        end
+      end
+    end
+    @hash
+  end
+
+  def print_console
+    @hash.each do |name, data|
+      puts name
+      @hash[name].each do |name_data, value_data|
+        print "#{name_data}: #{value_data}"
+        puts
+      end
+      puts "================================================================================================================="
+    end
+  end
+end
+
+data = Parserxls.new(ARGV[0])
+data.parse
+data.print_console
